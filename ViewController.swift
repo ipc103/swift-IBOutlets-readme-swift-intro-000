a2b029291d60d73641362ceccf19420e0f8a8201
ipc103 <iancandysemail@gmail.com>
2017-03-11 14:03:08 -0500

    add buttons and labels

--- ViewController.swift
+++ ViewController.swift
@@ -9,16 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var textDisplay: UILabel!
+    
+    func setTextDisplay (text: String) {
+        textDisplay.text = text
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setTextDisplay(text: "Ian Rules!")
     }
 
+    @IBAction func goodbyeWorld(_ sender: Any) {
+        setTextDisplay(text: "GoodBye")
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    @IBAction func hello(_ sender: Any) {
+        setTextDisplay(text: "Hello!")
+    }
 
+    @IBAction func unicornTouch(_ sender: Any) {
+        setTextDisplay(text: "Unicorn")
+    }
    
 
 }
